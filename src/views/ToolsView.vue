dd12cc921ec5783fe7d457d7098ad7ffa990218e
Ben Schulzke <benschulzke@gmail.com>
2022-03-12 00:50:16 -0700

    DRAFT: Partially functional calculator

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -19,8 +19,8 @@
                 </div>
                 <div class="incrementer">
                   <div>
-                    <div id="add-dice" class="plus" @click="addDice">&plus;</div>
-                    <div class="plus" @click="subtractDice">&minus;</div>
+                    <div id="add-dice" class="plus" v-on:click="numDice++">&plus;</div>
+                    <div class="plus" v-on:click="numDice--">&minus;</div>
                   </div>
                 </div>
               </div>
@@ -39,20 +39,50 @@
 // @ is an alias to /src
 
 export default {
-  name: 'RuleView',
+  name: 'ToolsView',
 
-  data() {
+  data: function() {
     return {
-        header: "Probability Charts",
-        paragraphs: [
-            {text: "These charts help you decide how many dice to roll for a given challenge"},
-            {text: "The tables show the nunber of dice rolled on the left, and on the right, the probability of rolling at least one of the target number(s)"},
-            {text: "The charts show the same info, graphed visually on a curve"},
-            {text: "For example, if you roll three dice, you've only got a 42% chance of rolling at least one 6, while on five dice, that goes up to 60%"},
-        ],
-        rawHtml: `<a href="">TESTING</a>`,
+        numDice: 1,
+        targets: 'six',
+        six: [0,17,31,42,52,60,67,72,77,81,84,87,88,90,92,94,94,95,96,97,97,98],
+        fiveOrSix: [0,31,52,67,77,84,89,92,95,96,97,99,99,99,99,99,99,99,],
+        fourFiveOrSix: [0,42,67,81,89,94,96,98,99,99,99.6,],
       }
-    }
+    },
+    computed: {
+      percentChance() {
+            if (this.targets === 'six') {
+                if (this.numDice < 22) {
+                    return this.six[this.numDice];
+                } else {
+                    return 99;
+                }
+            } else if (this.targets === 'fiveOrSix') {
+                if (this.numDice < 18) {
+                    return this.fiveOrSix[this.numDice];
+                } else {
+                    return 99.99;
+                } 
+            } else {
+                if (this.numDice < 11) {
+                    return this.fourFiveOrSix[this.numDice];
+                } else {
+                    return 99.99;
+                }
+            }
+        },
+        targetPhrase() {
+            if (this.targets === 'six') {
+                return "six";
+            } else if (this.targets === 'fiveOrSix') {
+                return "five or six";
+            } else if (this.targets === 'fourFiveOrSix') {
+                return "four, five or six";
+            }
+            return "";
+        }
+    },
   }
 </script>
 
