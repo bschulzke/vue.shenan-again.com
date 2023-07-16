99453a3eb2690dd08f1709129cb0381d4bf003b2
Ben Schulzke <benschulzke@gmail.com>
2023-07-16 14:11:19 -0600

    Begin restructuring site as rulebook

---
+++ src/components/RulesParagraph.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="paragraph-content">{{content}}</p>
+</template>
+
+<script>
+export default {
+  
+
+  name: 'RulesParagraph',
+  props: {
+    content: String
+  }
+}
+</script>
+
+<style scoped>
+  .paragraph-content {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+
+    @media only screen and (min-width: 901px) {
+      
+    }
+
+</style>
